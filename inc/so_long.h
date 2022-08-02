7175d78853614e2a1f8e2db2db9d7954b209e8af
Monine Chan <mochan@students.42wolfsburg.de>
2022-08-02 18:16:28 +0200

    draw pixel

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,9 +6,17 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:37:09 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/01 17:40:04 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/01 19:25:19 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include "./minilibx_opengl_20191021/mlx.h"
+#include "../minilibx_opengl_20191021/mlx.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
--- src/so_long.c
+++ src/so_long.c
@@ -6,18 +6,47 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:14:21 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/01 17:30:38 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/01 19:28:43 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "../inc/so_long.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+// int	deal_key(int key, void *param)
+// {
+// 	ft_putchar('X');
+// 	return (0);
+// }
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char *dst;
+
+	dst = data->addr + (y * data -> line_length + x *(data->bits_per_pixel / 8));
+	*(unsigned int*) dst = color;
+}
+
 
 int	main()
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
 	
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx_42");
-	mlx_loop(mlx_ptr);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	// win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hello World!");
+	// mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+	// mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_loop(mlx);
 }
+
