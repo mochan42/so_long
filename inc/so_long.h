6e8cfe0b33e88b0fe4ffa8d5ea85c47dd7c22ae3
Monine Chan <mochan@students.42wolfsburg.de>
2022-08-03 17:19:07 +0200

    display an image in a window

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:37:09 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/03 15:40:59 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/03 16:58:29 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,6 @@
 #include "../minilibx_opengl_20191021/mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct	s_image {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_image;
 
 typedef struct	s_prgm {
 	void	*mlx;
--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 15:14:21 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/03 15:47:20 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/03 17:15:53 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,21 @@ int	key_hook(int keycode, t_prgm *prgm)
 int	main()
 {	
 	t_prgm	prgm;
-	
+	void	*img;
+	int		width;
+	int		height;
+	char	path_floor_tile[]="./images/floor_tile.xpm";
+	int		floor_tile;
+
+	width = 16;
+	height = 16;
 	prgm.mlx = mlx_init();
-	prgm.win = mlx_new_window(prgm.mlx, 640, 480, "Hello world!");
+	prgm.win = mlx_new_window(prgm.mlx, 640, 480, "so long");
+	img = mlx_xpm_file_to_image(prgm.mlx, path_floor_tile, &width, &height);
+	floor_tile = mlx_put_image_to_window(prgm.mlx, prgm.win, img, 0, 0);
 	mlx_key_hook(prgm.win, key_hook, &prgm);
 	mlx_hook(prgm.win, 17, 0, ft_close_window, &prgm);
 	mlx_loop(prgm.mlx);
+	return (0);
 }
 
