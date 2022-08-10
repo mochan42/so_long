e6ca3301c07b85dd084372fda08576d292710eae
Monine Chan <mochan@students.42wolfsburg.de>
2022-08-10 10:36:13 +0200

    fix norminette line too long issue

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 21:20:22 by mochan            #+#    #+#             */
-/*   Updated: 2022/08/10 01:14:23 by mochan           ###   ########.fr       */
+/*   Updated: 2022/08/10 10:31:21 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ void	move_up(t_prgm *vr)
 	if (vr->map[vr->pl_ym - 1][vr->pl_xm] != '1')
 	{
 		vr->img = mlx_xpm_file_to_image(vr->mlx, pl_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw - 64);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw - 64);
 		vr->img = mlx_xpm_file_to_image(vr->mlx, ti_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw);
 		vr->pl_ym -= 1;
 		vr->pl_yw -= 64;
 	}
@@ -40,9 +42,11 @@ void	move_down(t_prgm *vr)
 	if (vr->map[vr->pl_ym + 1][vr->pl_xm] != '1')
 	{
 		vr->img = mlx_xpm_file_to_image(vr->mlx, pl_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw + 64);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw + 64);
 		vr->img = mlx_xpm_file_to_image(vr->mlx, ti_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw);
 		vr->pl_ym += 1;
 		vr->pl_yw += 64;
 	}
@@ -58,9 +62,11 @@ void	move_right(t_prgm *vr)
 	if (vr->map[vr->pl_ym][vr->pl_xm + 1] != '1')
 	{
 		vr->img = mlx_xpm_file_to_image(vr->mlx, pl_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw + 64, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw + 64,
+			vr->pl_yw);
 		vr->img = mlx_xpm_file_to_image(vr->mlx, ti_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw);
 		vr->pl_xm += 1;
 		vr->pl_xw += 64;
 	}
@@ -76,9 +82,11 @@ void	move_left(t_prgm *vr)
 	if (vr->map[vr->pl_ym][vr->pl_xm - 1] != '1')
 	{
 		vr->img = mlx_xpm_file_to_image(vr->mlx, pl_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw - 64, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw - 64,
+			vr->pl_yw);
 		vr->img = mlx_xpm_file_to_image(vr->mlx, ti_path, vr->wid, vr->hei);
-		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw, vr->pl_yw);
+		mlx_put_image_to_window(vr->mlx, vr->win, vr->img, vr->pl_xw,
+			vr->pl_yw);
 		vr->pl_xm -= 1;
 		vr->pl_xw -= 64;
 	}
